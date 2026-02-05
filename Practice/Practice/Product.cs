a3eafcef94aabbae14376c5dbf9af8cc570f0024
Menna <mennamokhtar621@gmail.com>
2026-02-05 22:58:07 +0200

    Add files via upload

---
+++ Practice/Practice/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Practice
+{
+   public class Product
+   {
+        private string name;
+        private double price;
+        private Category category;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+
+                if (value <= 0)
+                {
+                    Console.WriteLine("Invalid Price");
+                }
+                else
+                {
+                     price = value;
+                }
+            }
+        }
+
+        public Category Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+
+
+        public Product(string name , double price, Category category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        
+       public Location PLocation { get; set; }
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice
+{
+
+    public enum Category
+    {
+        Food,
+        Electronics,
+        Clothing
+    }
+
+    public struct Location
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product("phone", 20000, Category.Electronics);
+
+            Console.WriteLine(product.Name);
+
+            product.Price = -10;
+
+            Console.WriteLine(product.Price);
+        }
+    }
+}
+
+
+
